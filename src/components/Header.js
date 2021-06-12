bc6dd96e111e491bdbbb38dd24ce7a7c3f6ad063
artginzburg <art.ginzburg@ya.ru>
2021-06-12 18:06:30 +0300

    change link in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
 
-      <a target="_self" href="https://github.com/artginzburg/mesto" className="logo header__logo">
+      <a target="_self" href="https://github.com/artginzburg/mesto-react" className="logo header__logo">
         <img className="logo__img" alt="Mesto" src={logo} />
       </a>
 
