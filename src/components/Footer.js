b4a26dd21dcf41f4d9c9e4e3fa43540bb939e28a
artginzburg <art.ginzburg@ya.ru>
2021-06-18 09:44:47 +0300

    remove excessive eslint config, import Footer author from package.json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import { author } from '../../package.json';
+
 function Footer() {
   return (
     <footer className="footer">
 
-      <a target="_blank" rel="noreferrer" href="https://github.com/artginzburg" className="footer__copyright">&copy; 2021 Art Ginzburg</a>
+      <a target="_blank" rel="noreferrer" href="https://github.com/artginzburg" className="footer__copyright">&copy; 2021 {author}</a>
 
     </footer>
   );
