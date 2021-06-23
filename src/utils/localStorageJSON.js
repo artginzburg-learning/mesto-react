feb6cc78d2d3d3311c519783d0a3d22183901f95
artginzburg <art.ginzburg@ya.ru>
2021-06-23 20:20:26 +0300

    fix excessive updates due to localStorageJSON

--- src/utils/localStorageJSON.js
+++ src/utils/localStorageJSON.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
-function initiateLocalStorageJSON(computed, defaultForComputed) {
-  return {
-    get [computed]() {
-      return localStorage[computed] ? JSON.parse(localStorage[computed]) : defaultForComputed;
-    },
-    set [computed](newValue) {
-      return localStorage[computed] = JSON.stringify(newValue);
-    }
-  };
-}
-
 export default function useStateWithLocalStorage(computed, defaultForComputed) {
-  const localStorageJSON = initiateLocalStorageJSON(computed, defaultForComputed);
-
-  const [currentState, setCurrentState] = React.useState(localStorageJSON[computed]);
+  const [currentState, setCurrentState] = React.useState(
+    localStorage[computed]
+      ? JSON.parse(localStorage[computed])
+      : defaultForComputed
+  );
 
   React.useEffect(() => {
-    localStorageJSON[computed] = currentState;
-  }, [currentState, localStorageJSON, computed]);
+    console.log('saving state of ' + computed + ' to localStorage');
+    localStorage[computed] = JSON.stringify(currentState);
+  }, [currentState, computed]);
 
   return [currentState, setCurrentState];
 }
